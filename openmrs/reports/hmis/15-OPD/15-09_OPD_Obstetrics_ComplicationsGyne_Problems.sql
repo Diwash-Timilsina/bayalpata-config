34b6ca466e335319019824f1698f11456c0d2f3a
Diwash <timilsina.dwas@gmail.com>
2021-07-13 08:32:56 +0545

    Update 15-09_OPD_Obstetrics_ComplicationsGyne_Problems.sql
    Old query is replaced after updating standard dataset according to HMIS format

--- openmrs/reports/hmis/15-OPD/15-09_OPD_Obstetrics_ComplicationsGyne_Problems.sql
+++ openmrs/reports/hmis/15-OPD/15-09_OPD_Obstetrics_ComplicationsGyne_Problems.sql
@@ -1,3 +1,4 @@
+
 SELECT 
     first_answers.answer_name AS 'ICD name',
     first_answers.icd10_code AS 'ICD CODE',
@@ -9,16 +10,23 @@ SELECT
                     1
                 ELSE 0
             END),
-            0) AS 'FEMALE PATIENT'
-
- 
+            0) AS 'FEMALE PATIENT',
+    IFNULL(SUM(CASE
+                WHEN
+                    second_concept.gender = 'M'
+                        AND second_concept.person_id IS NOT NULL
+                THEN
+                    1
+                ELSE 0
+            END),
+            0) AS 'MALE PATIENT'
 FROM
     (SELECT 
         concept_full_name AS answer_name, icd10_code
     FROM
         diagnosis_concept_view
     WHERE
-        icd10_code IN ('O00.9','O08.9','O13','O14.1','O15.0','O15.1','O15.2','O21.0','O46.9','O63.0','O65.9','O71.1','O72','O73','O74.9','O85','N73.9','N81.4','N92.6','N93.9','N97.9')) first_answers
+        icd10_code IN ('O00.9','O08.9','O13','O14.1','O15.0','O15.1','O15.2','O21.0','O46.9','O63.0','O65.9','O71.1','O72','O73','O75.9','O85','N73.9','N81.4','N92.6','N93.9','N97.9','N46')) first_answers
         LEFT OUTER JOIN
     (SELECT DISTINCT
         (p.person_id),
@@ -40,30 +48,19 @@ FROM
         AND o.voided = 0
         AND cn.voided = 0
      JOIN diagnosis_concept_view dcv ON dcv.concept_id = o.value_coded
-        AND dcv.icd10_code IN ('O00.9','O08.9','O13','O14.1','O15.0','O15.1','O15.2','O21.0','O46.9','O63.0','O65.9','O71.1','O72','O73','O74.9','O85','N73.9','N81.4','N92.6','N93.9','N97.9')
+        AND dcv.icd10_code IN ('O00.9','O08.9','O13','O14.1','O15.0','O15.1','O15.2','O21.0','O46.9','O63.0','O65.9','O71.1','O72','O73','O75.9','O85','N73.9','N81.4','N92.6','N93.9','N97.9','N46')
     WHERE
         p.voided = 0) first_concept ON first_concept.icd10_code = first_answers.icd10_code
         LEFT OUTER JOIN
     (SELECT DISTINCT
         (person.person_id) AS person_id,
-            cn2.concept_id AS answer,
-            obs.concept_id AS question,
-            obs.obs_datetime AS datetime,
             visit.visit_id AS visit_id,
             person.gender AS gender
-    FROM
-        obs
-    INNER JOIN concept_view question ON obs.concept_id = question.concept_id
-        AND question.concept_full_name IN ('Department Sent To')
-    INNER JOIN concept_name cn2 ON obs.value_coded = cn2.concept_id
-        AND cn2.concept_name_type = 'FULLY_SPECIFIED'
-  AND UPPER(cn2.name) NOT Like '%EMERGENCY%'
-    INNER JOIN person ON obs.person_id = person.person_id
-    INNER JOIN encounter ON obs.encounter_id = encounter.encounter_id
-    INNER JOIN visit ON encounter.visit_id = visit.visit_id
+   FROM person 
+     JOIN visit  ON person_id = visit.patient_id 
+     JOIN visit_type vt ON visit.visit_type_id = vt.visit_type_id AND vt.name = 'OPD'
     WHERE
-        CAST(obs.obs_datetime AS DATE) BETWEEN DATE('#startDate#') AND DATE('#endDate#')) second_concept ON first_concept.person_id = second_concept.person_id
+        cast(visit.date_started AS DATE) BETWEEN DATE('#startDate#') AND DATE('#endDate#')) second_concept ON first_concept.person_id = second_concept.person_id
         AND first_concept.visit_id = second_concept.visit_id
 GROUP BY first_answers.icd10_code
-ORDER BY FIELD(first_answers.icd10_code,'O00.9','O08.9','O13','O14.1','O15.0','O15.1','O15.2','O21.0','O46.9','O63.0','O65.9','O71.1','O72','O73','O74.9','O85','N73.9','N81.4','N92.6','N93.9','N97.9')
-
+ORDER BY FIELD(first_answers.icd10_code, 'O00.9','O08.9','O13','O14.1','O15.0','O15.1','O15.2','O21.0','O46.9','O63.0','O65.9','O71.1','O72','O73','O75.9','O85','N73.9','N81.4','N92.6','N93.9','N97.9','N46')
